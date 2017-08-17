1ba15e5be9b8bf3b877066c694e2a5d907d649ef
Manuel Pineault <manuel.pineault@toptal.com>
2017-08-17 07:13:03 -0400

    feat: added Book.js new component
    Added new Book.js to allow for flexibility for search page. Refactored BookShelf.js to allow for Book.js component

---
+++ src/Book.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+
+class Book extends Component {
+  render() {
+    const book = this.props.book
+    const onUpdateBookShelf = this.props.onUpdateBookShelf
+    const updating = this.props.updating
+
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("' + book.imageLinks.smallThumbnail + '")' }}></div>
+          <div className={"book-shelf-changer " + (updating ? 'hidden' : '')}>
+            <select
+              onChange={(e) => onUpdateBookShelf(book, e.target.value)}
+              value={book.shelf}
+              disabled={updating}
+              style={{ cursor: ((updating) ? 'default' : 'pointer') }}
+              >
+              <option value="none" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">
+          {book.authors.map((author, key) =>
+            (<span key={author}>
+              {((book.authors.length > 1 && (key + 1) < book.authors.length) ? author + ', ': author)}
+            </span>)
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Book
--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Book from './Book'
 
 class BookShelf extends Component {
   render() {
@@ -14,33 +15,11 @@ class BookShelf extends Component {
           <ol className="books-grid">
             {books.map((book) => (
               <li key={book.id}>
-                <div className="book">
-                  <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("' + book.imageLinks.smallThumbnail + '")' }}></div>
-                    <div className={"book-shelf-changer " + (updating ? 'hidden' : '')}>
-                      <select
-                        onChange={(e) => onUpdateBookShelf(book, e.target.value)}
-                        value={book.shelf}
-                        disabled={updating}
-                        style={{ cursor: ((updating) ? 'default' : 'pointer') }}
-                        >
-                        <option value="none" disabled>Move to...</option>
-                        <option value="currentlyReading">Currently Reading</option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="book-title">{book.title}</div>
-                  <div className="book-authors">
-                    {book.authors.map((author, key) =>
-                      (<span key={author}>
-                        {((book.authors.length > 1 && (key + 1) < book.authors.length) ? author + ', ': author)}
-                      </span>)
-                    )}
-                  </div>
-                </div>
+                <Book
+                  book={book}
+                  onUpdateBookShelf={onUpdateBookShelf}
+                  updating={updating}
+                />
               </li>
             ))}
           </ol>
